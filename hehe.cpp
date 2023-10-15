ae8dade480d4742e82c0230a9d098439a7e8ca3e
ngxx-fus <msnp@outlook.com.vn>
2023-10-15 10:39:46 +0700

    test

---
+++ hehe.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>//toan bo thu vien chuan
+using namespace std;
+
+
+int main(){
+    cout << "Hello world!\n";
+    string s = "khoi nhe anh";
+    cout << s << '\n';
+    system("pause");
+return 0;
+}
