9de7039ae1a3daa2a285a3c5c80259bbc634d80a
ngxx-fus <msnp@outlook.com.vn>
2023-10-23 22:34:22 +0700

    23102013.001

--- Customer/templateClass.h
+++ Customer/templateClass.h
@@ -12,23 +12,25 @@ using namespace std ;
 class templateClass : public QListWidgetItem
 {
 protected:
-    typedef  unsigned int uInt;
+    QVector< QChar > int_to_QChar = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 private:
     QString name;
     QString ID;
     QString path; // path to file
     QString BOD;  //birth of day
 
-    bool checkDate(uInt DD, uInt MM, uInt YYYY){
-        //                    Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
-        QVector<uInt> normalYear = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        QVector<uInt> leapYear = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if( (YYYY % 100!=0)  && (YYYY % 4 == 0) )
-            return bool(leapYear.at(MM) == DD);
-        return bool(normalYear.at(MM) == DD);
-    }
-
 public:
+
+    bool checkDate(int DD, int MM, int YYYY){
+        if( DD < 1 || MM < 1 || YYYY < 1901 || YYYY > 2052 || MM > 12) return false;
+        //                    Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+        QVector<int> normalYear = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        QVector<int> leapYear = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if( (YYYY % 100!=0)  && (YYYY % 4 == 0) )
+            return bool(leapYear.at(MM) >= DD);
+        
+        return bool(normalYear.at(MM) >= DD);
+    }
 
     templateClass(QString name = "none",
                   QString ID = "none",
@@ -45,35 +47,35 @@ public:
         this->BOD = BOD;
     }
 
-    virtual void setBOD(uInt DD, uInt MM, uInt YYYY){
+    virtual void setBOD(int DD, int MM, int YYYY){
         if( checkDate(DD,MM,YYYY) == false ) return;
         this->BOD.clear();
-        if( DD < 10) {
-            this->BOD.push_back(QChar('0'));
-            this->BOD.push_back(QChar('0'+DD));
-        }else{
-            this->BOD.push_back(QChar('0'+DD/10));
-            this->BOD.push_back(QChar('0'+DD%10));
-        }
+        //DATE
+        this->BOD.push_back( int_to_QChar.at(DD/10) );
+        this->BOD.push_back( int_to_QChar.at(DD%10) );
+
         this->BOD.push_back(QChar('/'));
+
+        //MONTH
         if( MM < 10){
             this->BOD.push_back(QChar('0'));
-            this->BOD.push_back(QChar('0'+ MM));
+            this->BOD.push_back( int_to_QChar.at(MM) );
         }else{
-            this->BOD.push_back(QChar('1'));
+            this->BOD.push_back( QChar('1') );
             this->BOD.push_back( QChar((MM == 11)?('1'):('2')) );
         }
         this->BOD.push_back(QChar('/'));
-        this->BOD.push_back(QChar('0' + YYYY/1000));
+        this->BOD.push_back( int_to_QChar.at(YYYY/1000) );
         YYYY%=1000;
-        this->BOD.push_back(QChar('0' + YYYY%100));
+        this->BOD.push_back( int_to_QChar.at(YYYY/100) );
         YYYY%=100;
-        this->BOD.push_back(QChar('0' + YYYY%10));
+        this->BOD.push_back( int_to_QChar.at( YYYY/10) );
         YYYY%=10;
-        this->BOD.push_back(QChar('0' + YYYY));
+        this->BOD.push_back( int_to_QChar.at(YYYY) );
     }
 
-    virtual void setName(QString newName) {
+    virtual void setName(QString newName) 
+    {
         this->name = newName;
     }
 
--- build-Customer-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/Customer_autogen/include/ui_myCustomer.h
+++ build-Customer-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/Customer_autogen/include/ui_myCustomer.h
@@ -17,6 +17,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -32,6 +33,9 @@ public:
     QLabel *customer_ID;
     QLabel *customer_point;
     QLabel *customer_BOD;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
     QMenuBar *menubar;
     QMenu *menuCusManager;
     QStatusBar *statusbar;
@@ -45,22 +49,31 @@ public:
         centralwidget->setObjectName("centralwidget");
         listWidget = new QListWidget(centralwidget);
         listWidget->setObjectName("listWidget");
-        listWidget->setGeometry(QRect(30, 40, 651, 291));
+        listWidget->setGeometry(QRect(31, 40, 591, 291));
         customer_name = new QLabel(centralwidget);
         customer_name->setObjectName("customer_name");
-        customer_name->setGeometry(QRect(110, 20, 151, 20));
+        customer_name->setGeometry(QRect(111, 20, 151, 20));
         customer_numberphone = new QLabel(centralwidget);
         customer_numberphone->setObjectName("customer_numberphone");
-        customer_numberphone->setGeometry(QRect(38, 20, 61, 20));
+        customer_numberphone->setGeometry(QRect(351, 20, 91, 20));
         customer_ID = new QLabel(centralwidget);
         customer_ID->setObjectName("customer_ID");
-        customer_ID->setGeometry(QRect(380, 20, 121, 16));
+        customer_ID->setGeometry(QRect(51, 20, 51, 20));
         customer_point = new QLabel(centralwidget);
         customer_point->setObjectName("customer_point");
-        customer_point->setGeometry(QRect(490, 20, 121, 16));
+        customer_point->setGeometry(QRect(471, 20, 121, 16));
         customer_BOD = new QLabel(centralwidget);
         customer_BOD->setObjectName("customer_BOD");
-        customer_BOD->setGeometry(QRect(280, 20, 91, 20));
+        customer_BOD->setGeometry(QRect(251, 20, 91, 20));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(71, 350, 80, 24));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(240, 350, 101, 24));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName("pushButton_3");
+        pushButton_3->setGeometry(QRect(460, 350, 101, 24));
         myCustomer->setCentralWidget(centralwidget);
         menubar = new QMenuBar(myCustomer);
         menubar->setObjectName("menubar");
@@ -81,12 +94,15 @@ public:
 
     void retranslateUi(QMainWindow *myCustomer)
     {
-        myCustomer->setWindowTitle(QCoreApplication::translate("myCustomer", "myCustomer", nullptr));
+        myCustomer->setWindowTitle(QCoreApplication::translate("myCustomer", "Customers Management", nullptr));
         customer_name->setText(QCoreApplication::translate("myCustomer", "Customer's name", nullptr));
-        customer_numberphone->setText(QCoreApplication::translate("myCustomer", "ID", nullptr));
-        customer_ID->setText(QCoreApplication::translate("myCustomer", "Number phone", nullptr));
+        customer_numberphone->setText(QCoreApplication::translate("myCustomer", "Phone number", nullptr));
+        customer_ID->setText(QCoreApplication::translate("myCustomer", "ID", nullptr));
         customer_point->setText(QCoreApplication::translate("myCustomer", "Accumulation point", nullptr));
         customer_BOD->setText(QCoreApplication::translate("myCustomer", "Birth", nullptr));
+        pushButton->setText(QCoreApplication::translate("myCustomer", "Add customer", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("myCustomer", "Delete customer", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("myCustomer", "Edit Infomation", nullptr));
         menuCusManager->setTitle(QCoreApplication::translate("myCustomer", "Customer Manager", nullptr));
     } // retranslateUi
 
