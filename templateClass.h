b6147f2a2d88cf92b4c7fbc33fa38a52b77288bb
ngxx-fus <msnp@outlook.com.vn>
2023-10-29 23:49:32 +0700

    v291023.ngxxfus - add updateLatestModification() | update declaration of function generate_ID() |
    updateLatestModification() : recommand to use after modify any thing of _customers opject
    
    generate_ID(prefix, date_, time_, sufix_)  :
    - QString prefix, ""
    - date_ is active if *true*, else inactive
    - time_ is active if *true*, else inactive
    - QString sufix_ = ""

--- templateClass.h
+++ templateClass.h
@@ -1,11 +1,13 @@
 #ifndef TEMPLATECLASS_H
 #define TEMPLATECLASS_H
 
+
 #include <QListWidgetItem>
 #include <QtGui>
 #include <QtWidgets>
 #include <QTableWidget>
 #include <QMainWindow>
+#include <math.h>
 #include "_time.h"
 
 using namespace std ;
@@ -32,20 +34,29 @@ public:
     }
 
     //format: YYYYMMDDHHMMSS - MM 1st is month,  MM 2nd is minute
-    QString generate_ID(QString text = ""){
+    QString generate_ID(QString prefix_ = "", bool date_ = true, bool time_ = true, QString sufix_ = ""){
         static int count_t = 1;
         if(count_t > 100001) count_t  = 0;
         QString res;
         _time _t;
         _t.set_current_date_time();//set current time
-        res += _t.get_year() + _t.get_month() + _t.get_day();
-        res.push_back(QChar('.'));
-        res += _t.get_hour() + _t.get_minute() + _t.get_second();
-        res.push_back(QChar('.'));
-        res += _t.int_to_QString((count_t = (++count_t*123)%100001));
-        if(text.size()){
+        if(prefix_.size()){
+            res += prefix_;
             res.push_back(QChar('.'));
-            res += text;
+        }
+        if(date_) {
+            res += _t.get_year() + _t.get_month() + _t.get_day();
+            res.push_back(QChar('.'));
+        }
+        if(time_){
+            res += _t.get_hour() + _t.get_minute() + _t.get_second();
+            res.push_back(QChar('.'));
+        }
+
+        res += _t.int_to_QString((count_t = (++count_t*12*count_t)%100001));
+        if(sufix_.size()){
+            res.push_back(QChar('.'));
+            res += sufix_;
         }
         return res;
     }
@@ -84,6 +95,11 @@ public:
 
     virtual QString getPath() {
         return this->path;
+    }
+
+    void updateLatestModification()
+    {
+        this->latestModification.set_current_date_time();
     }
 
     _time getLatestModification(){
