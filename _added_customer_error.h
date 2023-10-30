27af7e68a210665647612828d16f241429088099
ngxx-fus <msnp@outlook.com.vn>
2023-10-30 08:39:34 +0700

    v301023.4 - del *.cpp

--- _added_customer_error.h
+++ _added_customer_error.h
@@ -1,6 +1,7 @@
 #ifndef _ADDED_CUSTOMER_ERROR_H
 #define _ADDED_CUSTOMER_ERROR_H
 
+#include "ui__added_customer_error.h"
 #include <QDialog>
 
 namespace Ui {
@@ -12,8 +13,18 @@ class _added_customer_error : public QDialog
     Q_OBJECT
 
 public:
-    explicit _added_customer_error(QWidget *parent = nullptr);
-    ~_added_customer_error();
+    explicit _added_customer_error(QWidget *parent) :
+        QDialog(parent),
+        ui(new Ui::_added_customer_error)
+    {
+        ui->setupUi(this);
+    }
+
+    ~_added_customer_error()
+    {
+        delete ui;
+    }
+
 
 private:
     Ui::_added_customer_error *ui;
